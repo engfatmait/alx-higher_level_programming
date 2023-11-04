d38d783a06f56e818c69c3528a1537b1e313db93
engfatmait <eng_fatma_it@yahoo.com>
2023-11-04 11:11:36 -0700

    task 5 return statement to left

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for x in range(len(my_string)):
         if (my_string[x] != 'c' or my_string[x] != 'C'):
             new_string += my_string[x]
-        return new_string
+    return new_string
