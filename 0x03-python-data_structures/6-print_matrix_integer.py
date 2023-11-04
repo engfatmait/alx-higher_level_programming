fc167d138bdd299aa7c0153ebb180bcdbdc4178e
engfatmait <eng_fatma_it@yahoo.com>
2023-11-04 11:53:11 -0700

    distance between columns

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        pass
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end = ' ')
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end = '')
         print()
