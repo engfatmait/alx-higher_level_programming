539963f034d5e73fb2d2e8e87eb577615dd22240
engfatmait <eng_fatma_it@yahoo.com>
2023-11-04 07:27:42 -0700

    task 3 print the new_list not my_list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     new_list = reversed(my_list)
     for x in range(len(new_list)):
-        print("{:d}".format(my_list[x]))
+        print("{:d}".format(new_list[x]))
