7fe8b80690aabd05a451782802f0b680a26a4293
engfatmait <eng_fatma_it@yahoo.com>
2024-07-28 01:46:34 -0700

    task 3 semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv[2] === undefined){
+	//checks if there is no argument
 	console.log('No argument');
 }else {
+	//print the argument value
 	console.log(process.argv[2]);
 }
