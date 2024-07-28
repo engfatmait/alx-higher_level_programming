c8b5745be21a4282d3b51ea745a6ace593580907
engfatmait <eng_fatma_it@yahoo.com>
2024-07-28 03:07:06 -0700

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,11 +2,12 @@
 
 // This line specifies the path to the interpreter for the script.
 
-if (isNaN(process.argv[2]) || process.argv[2] === undefined){
-	// Checks if the first argument is not a number.
-	console.log('Not a number');
-	// Print it is not a number.
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
+  // If the command-line argument is not a number or is undefined,
+  // print 'Not a number'.
 } else {
-	console.log('My number: ' + parseInt(process.argv[2]));
-	// print the number converted to an integer.
+  console.log('My number:', parseInt(process.argv[2]));
+  // If the argument is a valid number, print 'My number:' followed by,
+  // the parsed integer value.
 }
