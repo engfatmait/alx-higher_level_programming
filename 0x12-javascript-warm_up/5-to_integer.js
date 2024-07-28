163fdcdd415b34bffc0e1d91c4ccbf8890878e24
engfatmait <eng_fatma_it@yahoo.com>
2024-07-28 03:03:26 -0700

    task 5 semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,7 +6,7 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined){
 	// Checks if the first argument is not a number.
 	console.log('Not a number');
 	// Print it is not a number.
-}else {
+} else {
 	console.log('My number: ' + parseInt(process.argv[2]));
 	// print the number converted to an integer.
 }
