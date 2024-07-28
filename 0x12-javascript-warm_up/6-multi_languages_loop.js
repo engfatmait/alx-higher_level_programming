69efed83d592c2b21fb82d425de08c182f52b98f
engfatmait <eng_fatma_it@yahoo.com>
2024-07-28 03:27:53 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,9 +2,9 @@
 
 // This line specifies the path to the interpreter for the script.
 
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']){
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
 	// Iterates over array of elements.
 	// ['C is fun', 'Python is cool', 'JavaScript is amazing'].
 	console.log(line);
-	// Print the elements.
+	// Prints the elements.
 }
