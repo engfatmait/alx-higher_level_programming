0bc72949582c777844274d88fd3f90539152c439
engfatmait <eng_fatma_it@yahoo.com>
2024-07-28 01:57:22 -0700

    task4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+//this line specifies the path to interpreter
+
+console.log(process.argv[2] + 'is' + process.argv[3]);
+// prints two arguments passed to it, in the following format: “ is ”
